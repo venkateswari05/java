723b13ef1930f17a1e9be7a4fb8f3d7216f61526
venkateswari <parimivenkateswari@gmail.com>
2021-06-02 17:44:52 +0530

    addition program added

---
+++ java programs/Addition.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+  class Addition {
+
+	public static void main(String[] args) {
+
+      int a,b,c;
+      Scanner sc=new Scanner(System.in);
+      a=sc.nextInt();
+      b=sc.nextInt();
+      c=a+b;
+      System.out.println(c); 
+      sc.close();
+	}
+
+}
